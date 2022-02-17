d9a84795024f25bc7cbed189a338a37080e4365a
Glitch (kiwi-fish-antarctopelta) <none>
2022-02-17 12:08:37 +0000

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App flex flex-col justify-center items-center h-screen w-screen container mx-auto">
+      <h1 className="text-4xl text-indigo-600 font-bold">Init</h1>
+    </div>
+  );
+}
+
+export default App;
