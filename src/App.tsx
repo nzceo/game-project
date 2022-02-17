a73d63860470728a3722ce6e6d33d537a391b616
Glitch (kiwi-fish-antarctopelta) <none>
2022-02-17 12:45:14 +0000

    Initial game functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import useGame from './hooks/useGame'
 
 function App() {
+  const { gameState, turn } = useGame();
   return (
-    <div className="App flex flex-col justify-center items-center h-screen w-screen container mx-auto">
-      <h1 className="text-4xl text-indigo-600 font-bold">Init</h1>
+    <div>
+      <div>{gameState.display.map((display: any) => display.text)}</div>
+      <div>
+        {gameState.options.map((option: any) => (
+          <button
+            onClick={() => {
+              option.action();
+              turn();
+            }}
+          >
+            {option.text}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
